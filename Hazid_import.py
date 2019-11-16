568438c27fc2809d944fc50d63cc999b24a316bb
Alext <alexthecool1996@hotmail.co.uk>
2019-11-16 19:24:52 +0000

    Friday Nov 11 update

--- Hazid_import.py
+++ Hazid_import.py
@@ -5,7 +5,7 @@ from py2neo import Graph, Node, Relationship
 from collections import defaultdict
 
 ## TODO ; Create a non local host for the graph database
-graph = Graph(password="mAES12081604")
+# graph = Graph(password="mAES12081604")
 
 # https://docs.google.com/spreadsheets/d/1B9ZVkqULJcgF_PpkMap1fGj-hAowC1EIhO4wVeY7LBg/edit?usp=sharing
 # Generic Google drive API access
@@ -13,7 +13,7 @@ scope = ['https://spreadsheets.google.com/feeds',
          'https://www.googleapis.com/auth/drive']
 creds = ServiceAccountCredentials.from_json_keyfile_name('jsonsecretdata.json', scope)
 gc = gspread.authorize(creds)
-wks = gc.open("HAZOP_DATA").sheet1
+wks = gc.open("HAZOP_DATA").worksheet('HAZID')
 
 # Find guide words and deviation words from columns in HAZOP spreadsheet
 guide = wks.col_values(1)
@@ -21,24 +21,24 @@ guide = wks.col_values(1)
 
 Hazards = []
 Barriers = []
-Top_events = []
+Undesired_events = []
 Consq = []
 Threats = []
 for row, e in enumerate(guide):
     if guide[row] == "Hazard":
         Hazards.append(wks.cell(row+1, 2).value)
-    elif guide[row] == "Top_event":
-        Top_events.append(wks.cell(row+1, 2).value)
-    elif guide[row] == "Causes":
-        temp_Causes = (wks.row_values(row+1))
-        num_Causes = len(temp_Causes)
+    elif guide[row] == "Undesired_event":
+        Undesired_events.append(wks.cell(row+1, 2).value)
+    elif guide[row] == "Threats":
+        temp_Threats = (wks.row_values(row+1))
+        num_Threats = len(temp_Threats)
     elif guide[row] == "Barriers":
-        for i in range(num_Causes):
+        for i in range(num_Threats):
             # print(wks.cell(row+1, i+1))
-            if temp_Causes[i] != "Causes":
-                # print(temp_Causes[i])
+            if temp_Threats[i] != "Threats":
+                # print(temp_Threats[i])
                 # print(wks.cell(row+1, i+1).value)
-                Threats.append([temp_Causes[i], wks.cell(row+1, i+1).value])
+                Threats.append([temp_Threats[i], wks.cell(row+1, i+1).value])
     elif guide[row] == "Consequences":
         temp_Conq = (wks.row_values(row+1))
         num_Conq = len(temp_Conq)
@@ -61,7 +61,7 @@ with open('Threats.p', 'wb') as fp:
     pickle.dump(Threats, fp, protocol=pickle.HIGHEST_PROTOCOL)
 with open('Consequences.p', 'wb') as fp:
     pickle.dump(Consq, fp, protocol=pickle.HIGHEST_PROTOCOL)
-with open('Top_events.p', 'wb') as fp:
-    pickle.dump(Top_events, fp, protocol=pickle.HIGHEST_PROTOCOL)
+with open('Undesired_events.p', 'wb') as fp:
+    pickle.dump(Undesired_events, fp, protocol=pickle.HIGHEST_PROTOCOL)
 with open('Hazards.p', 'wb') as fp:
     pickle.dump(Hazards, fp, protocol=pickle.HIGHEST_PROTOCOL)
--- Neo4j_graph_from_data.py
+++ Neo4j_graph_from_data.py
@@ -1,51 +1,47 @@
 from py2neo import Graph, Node, Relationship
 import pickle
+import pprint
 
 # MATCH (n) DETACH DELETE n
 # Threats created from Spreadsheet, order is Threat then the barriers
 
-Top_event = Node("Top_event", name="Loss of containment of liquid")
+Undesired_event = Node("Undesired_event", name="Loss of containment of liquid")
 with open('Threats.p', 'rb') as fp:
     Threats = pickle.load(fp)
 with open('Consequences.p', 'rb') as fp:
     Consequences = pickle.load(fp)
-with open('Top_events.p', 'rb') as fp:
-    Top_events = pickle.load(fp)
+with open('Undesired_events.p', 'rb') as fp:
+    Undesired_events = pickle.load(fp)
 with open('Hazards.p', 'rb') as fp:
     Hazards = pickle.load(fp)
 
-# for threat in Threats:
-#     print(Threats[threat][-1])
-# breakpoint()
 graph = Graph(password="mAES12081604")
-for event, e in enumerate(Top_events):
-    TE_node = Node("Top Event", name=Top_events[event])
+
+
+def flatten(x):
+    result = []
+    for el in x:
+        if hasattr(el, "__iter__") and not isinstance(el, str):
+            result.extend(flatten(el))
+        else:
+            result.append(el)
+    return result
+
+
+# for key in Threats.items():
+#     Threats_flat.append(flatten(key))
+
+
+for event, e in enumerate(Undesired_events):
+    TE_node = Node("Top Event", name=Undesired_events[event])
     # Create hazard
     HAZ_node = Node("Hazard", name=Hazards[event])
     # link hazard to top event
     HAZ = Relationship.type("Hazard")
-    graph.merge(HAZ(HAZ_node, TE_node), "Top_event", "name")
-    for threat in Threats:
-        temp_node = Node("Threat", name=threat)
-        for bar, j in enumerate(Threats[threat]):
-            # TODO connect barriers here
-            if j != "":
-                if bar == 0:
-                    THREAT = Relationship.type("BARRIER")
-                    temp_threat_node = Node("Barrier", name=j)
-                    graph.merge(THREAT(temp_node, temp_threat_node), "Barrier", "name")
-                elif bar < len(Threats[threat]):
-                    # print(Threats[threat][bar])
-                    BARRIER = Relationship.type("BARRIER")
-                    temp_threat_node = Node("Barrier", name=Threats[threat][bar-1])
-                    graph.merge(BARRIER(temp_threat_node, Node("Barrier", name=j)), "Barrier", "name")
-                if bar == len(Threats[threat])-1:
-                    THREAT = Relationship.type("THREAT")
-                    graph.merge(THREAT(TE_node, Node("Barrier", name = Threats[threat][-1])), "Barrier", "name")
+    graph.merge(HAZ(HAZ_node, TE_node), "Undesired_event", "name")
+    for item, i in enumerate(Threats_flat):
+        print(Threats_flat[item])
 
-                # graph.merge(THREAT)
-                # Threats[threat][bar]
-                # THREAT = Relationship.type("THREAT")
 
         # TODO connect last barrier to top event here
         # TODO iterate over these threats and produce barriers
