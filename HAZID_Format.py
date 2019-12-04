7c2c3331f9e3bf59e1d58719888301c13658b4c1
Al <alexthecool1996@hotmail.co.uk>
2019-12-04 01:38:35 +0000

    Fix for all problems

--- HAZID_Format.py
+++ HAZID_Format.py
@@ -9,7 +9,7 @@ scope = ['https://spreadsheets.google.com/feeds',
          'https://www.googleapis.com/auth/drive']
 creds = ServiceAccountCredentials.from_json_keyfile_name('jsonsecretdata.json', scope)
 gc = gspread.authorize(creds)
-wks = gc.open("HAZOP_DATA").worksheet('HAZID_CEXC')
+wks = gc.open("HAZOP_DATA").worksheet('Sheet4')
 
 Procedures = wks.col_values(1)
 Hazards = wks.col_values(2)
@@ -18,57 +18,19 @@ Consequences = wks.col_values(4)
 Threats = wks.col_values(6)
 temp_Mits = wks.col_values(5)
 temp_Bars = wks.col_values(7)
-temp_guide = []
-Consq = []
-Threat = []
-# TODO process this file in the same way Hazard_import does
 
-guide = wks.col_values(2)
-guide_length = len(guide)
-wks.update_cell(guide_length + 1, 2, "'")
-
-Haz_UE = []
-flat_list = []
-for i, e in enumerate(Hazards):
-    Haz_UE.append([Hazards[i], Undesired_events[i]])
-
-for i, Mitigation in enumerate(temp_Mits):
-    temp_Consq = Consequences[i]
-    if "." in Mitigation:
-        temp = [[temp_Consq], list(filter(None, Mitigation.split(".")))]
-        for sublist in temp:
-            for item in sublist:
-                flat_list.append(item)
-        Consq.append(flat_list)
-    else:
-        temp = [temp_Consq, Mitigation]
-        Consq.append(temp)
-    flat_list = []
-
-for j, Barrier in enumerate(temp_Bars):
-    temp_Threats = Threats[j]
-    if "." in Barrier:
-        temp1 = [[temp_Threats], list(filter(None, Barrier.split(".")))]
-        for sublist in temp1:
-            for item in sublist:
-                flat_list.append(item)
-        Threat.append(flat_list)
-    else:
-        temp1 = [temp_Threats, Barrier]
-        Threat.append(temp1)
-
-data = []
-for item in range(len(Hazards)):
-    if item > 0:
-        data.append([item, Haz_UE[item], [Consq[item]], [Threat[item]]])
-
-for mylist in data:
-    for sublist in mylist:
-        if type(sublist) == type(list()):
-            for key, val in enumerate(sublist[0]):
-                if sublist[0][key] == '':
-                    sublist[0].remove('')
-print(data[15:19])
+temp = []
+# Construct
+num_Haz = len(Hazards)-1
+for key in range(1, num_Haz+1):
+    # Get slice
+    # Ensure the last slice is obtained
+    # temp.append([[Undesired_events[key], Hazards[key]],
+    #              [Consequences[key], temp_Mits[key]],
+    #              [Threats[key], temp_Bars[key]]])
+    temp.append([[wks.cell(key+1, 2).value, wks.cell(key+1, 3).value],
+                 [wks.cell(key+1, 4).value, wks.cell(key+1, 5).value],
+                 [wks.cell(key+1, 6).value, wks.cell(key+1, 7).value]])
 
 with open('Data.p', 'wb') as fp:
-    pickle.dump(data, fp, protocol=pickle.HIGHEST_PROTOCOL)
+    pickle.dump(temp, fp, protocol=pickle.HIGHEST_PROTOCOL)
--- Neo4j_graph_from_data.py
+++ Neo4j_graph_from_data.py
@@ -6,56 +6,65 @@ import time
 
 with open('Data.p', 'rb') as fp:
     Data = pickle.load(fp)
-graph = Graph("bolt://localhost:7687", password="mAES12081604")
+graph = Graph("bolt://localhost:11002", password="12345")
 
 ### TODO This line of code erases the current graph if it exists. Comment it out if this is not required
 graph.run("MATCH (n) DETACH DELETE n")
 ###
 
 for event, row in enumerate(Data):
-    HAZ_node = Node("Hazard", name=("Hazard: "+row[1][0]))
-    TE_node = Node("Undesired_event", name=("Undesired_event: "+row[1][1]))
+    HAZ_node = Node("Hazard", name=("Hazard: "+row[0][0]))
+    UE_node = Node("Undesired_event", name=("U_e: "+row[0][1]))
     HAZ = Relationship.type("Hazard")
     CONSQ = Relationship.type("Consequence")
     THREAT = Relationship.type("Threat")
-    graph.merge(HAZ(HAZ_node, TE_node), "Undesired_event", "name")
-    for i, consq_row in enumerate(row[2]):
-        if "Mitigations" in consq_row:
-            consq_row.remove("Mitigations")
-        num_Consq = len(row[2][i])
-        Consq_node = Node("Consequence", name=(consq_row[0]))
-        if len(consq_row) == 1:
-            graph.merge(CONSQ(Consq_node, TE_node), "Consequence", "name")
-        elif len(consq_row) > 1:
-            for num, val in enumerate(consq_row):
-                if 1 <= num < (len(consq_row)):
-                    MIT = Relationship.type("Mitigation")
-                    MIT_node = Node("Mitigation", name=val)
-                    # print(num)
-                    MIT_node2 = Node("Mitigation", name=(consq_row[num-1]))
-                    graph.merge(MIT(MIT_node2, MIT_node), "Mitigation", "name")
-                if num == len(consq_row)-1:
-                    # print(consq_row[num])
-                    MIT_node3 = Node("Mitigation", name=(consq_row[num]))
-                    graph.merge(CONSQ(MIT_node3, TE_node), "Mitigation", "name")
+    graph.merge(HAZ(HAZ_node, UE_node), "Undesired_event", "name")
+    Consq_node = Node("Consequence", name=(row[1][0]))
+    Threat_node = Node("Threat", name=row[2][0])
+# Consequences at mitigations
 
-    for j, threat_row in enumerate(row[3]):
-        if "Barriers" in threat_row:
-            threat_row.remove("Barriers")
-        num_threat = len(row[3][j])
-        Threat_node = Node("Threat", name=(threat_row[0]))
-        if len(threat_row) == 1:
-            graph.merge(THREAT(Threat_node, TE_node), "Threat", "name")
-        elif len(threat_row) > 1:
-            for num, val in enumerate(threat_row):
-                if 1 <= num < (len(threat_row)):
-                    BAR = Relationship.type("Barrier")
-                    BAR_node = Node("Barrier", name=val)
-                    BAR_node2 = Node("Barrier", name=(threat_row[num-1]))
-                    graph.merge(BAR(BAR_node2, BAR_node), "Barrier", "name")
-                if num == len(threat_row)-1:
-                    BAR_node3 = Node("Barrier", name=(threat_row[num]))
-                    graph.merge(THREAT(BAR_node3, TE_node), "Barrier", "name")
+    if row[1][1] == '':
+        # If there is no Mitigation, then connect to UE
+        graph.merge(CONSQ(Consq_node, UE_node), "Consequence", "name")
+    elif '.' not in row[1][1]:
+        # If there is 1 Mitigation then Connect the mitigation to the UE and the Consq to the mitigation
+        Mit_node = Node("Mitigation", name=(row[1][1]))
+        graph.merge(CONSQ(Consq_node, Mit_node), "Mitigation", "name")
+        graph.merge(CONSQ(Mit_node, UE_node), "Mitigation", "name")
+    elif '.' in row[1][1]:
+        # If there is more than one Mitigation, split them up and chain them together. Length should always be > 2
+        temp = list(filter(None, row[1][1].split('.')))
+        for j in range(len(temp)):
+            if j == 0:
+                Mit_node = Node("Mitigation", name=temp[j])
+                graph.merge(CONSQ(Mit_node, UE_node), "Mitigation", "name")
+            elif j <= len(temp)-1:
+                Mit_node = Node("Mitigation", name=temp[j])
+                Mit_node2 = Node("Mitigation", name=temp[j-1])
+                graph.merge(CONSQ(Mit_node, Mit_node2), "Mitigation", "name")
+                if j == len(temp)-1:
+                    graph.merge(CONSQ(Consq_node, Mit_node), "Consequence", "name")
 
-# time.sleep(10)
-# graph.run("MATCH (n) RETURN n LIMIT 40")
+
+    if row[2][1] == '':
+        # If there is no Barrier, then connect to UE
+        graph.merge(THREAT(Threat_node, UE_node), "Threat", "name")
+    elif '.' not in row[2][1]:
+        # If there is 1 Barrier then Connect the barrier to the UE and the Threat to the barrier
+        Bar_node = Node("Barrier", name=(row[2][1]))
+        graph.merge(THREAT(Threat_node, Bar_node), "Barrier", "name")
+        graph.merge(THREAT(Bar_node, UE_node), "Barrier", "name")
+    elif '.' in row[2][1]:
+        # If there is more than one Barrier, split them up and chain them together. Length should always be > 2
+        temp = list(filter(None, row[2][1].split('.')))
+        for j in range(len(temp)):
+            if j == 0:
+                Bar_node = Node("Barrier", name=temp[j])
+                graph.merge(THREAT(Bar_node, UE_node), "Barrier", "name")
+            elif j <= len(temp):
+                Bar_node = Node("Barrier", name=temp[j])
+                Bar_node2 = Node("Barrier", name=temp[j-1])
+                graph.merge(THREAT(Bar_node, Bar_node2), "Barrier", "name")
+                if j == len(temp)-1:
+                    graph.merge(THREAT(Threat_node, Bar_node), "Threat", "name")
+
